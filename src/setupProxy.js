424208f17e44019bda2f25505775ec9c4492f769
noel <noel.camara@etu.univ-lehavre.fr>
2022-03-18 15:59:34 +0100

    Ajout du fichier setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,27 +1,26 @@
-// const proxy = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//   var wsProxy = proxy('/socket', {
-//     target: 'http://localhost:1234',
-//     changeOrigin: true, // for vhosted sites, changes host header to match to target's host
-//     ws: true, // enable websocket proxy
-//     logLevel: 'debug'
-//   })
-//   app.use(wsProxy);
-// };
-
-
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/socket',
-    createProxyMiddleware({
-      target: 'http://localhost:1234',
-      changeOrigin: true,
+  var wsProxy = proxy('/socket', {
+    target: 'http://localhost:1234',
+    changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+    ws: true, // enable websocket proxy
+    logLevel: 'debug'
+  })
+  app.use(wsProxy);
+};
+
+
+//const { createProxyMiddleware } = require('http-proxy-middleware');
+
+//module.exports = function(app) {
+//  app.use(
+//    '/socket',
+//    createProxyMiddleware({
+//      target: 'http://localhost:1234',
+//      changeOrigin: true,
       // ws: true, // enable websocket proxy
       // logLevel: 'debug'
-    })
-  );
-};
+//    })
+//  );
+//};
